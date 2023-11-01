6d323aeca96d27e0b4557dc8ab559d466a654117
Clovis Vinicius Avelar <avelarvnc@gmail.com>
2023-11-01 10:16:47 -0300

    init

---
+++ classes/Cachorro.php
@@ -0,0 +1,43 @@
+<?php
+
+    class Cachorro
+    {
+        //definindo os atributos da classe
+        private $nome;
+        private $raca;
+        private $cor;
+        private $peso;
+        private $idade;
+        private $dono;
+
+        //definindo o método construtor que irá criar objetos dessa classe
+        public function __construct() {}
+
+        public function create($_nome, $_raca, $_cor, $_peso, $_idade, $_dono)
+        {
+            $this->nome = $_nome;
+            $this->raca = $_raca;
+            $this->cor = $_cor;
+            $this->peso = $_peso;
+            $this->idade = $_idade;
+            $this->dono = $_dono;
+
+        }
+
+        //funções específicas da classe
+
+        public function latir()
+        {
+            echo "$this->nome fez AU AU! <br>";
+        }
+
+        public function rosnar($_pessoa)
+        {
+            echo $this->dono == $_pessoa ? 
+                                "$this->nome abanou o rabo. <br>" : 
+                                "$this->nome rosnou para você. <br>";
+        }
+
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="">
+    
+        <label for="nome">Nome</label>
+        <input type="text" name="nome">
+        <br>
+        <label for="nome">Raça</label>
+        <input type="text" name="raca">
+        <br>        
+        <label for="nome">Cor</label>
+        <input type="text" name="cor">
+        <br>        
+        <label for="nome">Peso</label>
+        <input type="text" name="peso">
+        <br>        
+        <label for="nome">Idade</label>
+        <input type="text" name="idade">
+        <br>
+        <label for="nome">Dono</label>
+        <input type="text" name="dono">
+        <br>
+        <input type="submit" value="Salvar">
+    </form>
+
+    <?php
+        include("classes/Cachorro.php");
+
+        // $dog = new Cachorro(); //instanciando um objeto da classe Cachorro
+        // $dog->create("Spike", "Vira Lata", "Caramelo", 5, 6, "Diogo");
+
+        // $dog->latir();
+
+        // $dog2 = new Cachorro();
+        // $dog2->create("Tobias", "Shiba Inu", "Amarelo", 3, 1, "Moises");
+
+        // $dog2->latir();
+        
+        // $dog->rosnar("Diogo");
+        // $dog->rosnar("Luiz");
+
+        // $dog2->rosnar("Moises");
+        // $dog2->rosnar("Julia");
+
+        if (isset($_GET["nome"]))
+        {
+            $dog3 = new Cachorro();
+            
+            $dog3->create($_GET["nome"], $_GET["raca"], $_GET["cor"], 
+                            $_GET["peso"], $_GET["idade"], $_GET["dono"]);
+
+            $dog3->latir();
+        }
+
+    ?>
+    
+</body>
+</html>
